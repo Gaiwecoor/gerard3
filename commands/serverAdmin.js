59cb50dad1d4ff0646fd665c2e85aacf5e8a9014
Gaiwecoor <gaiwecoor@gmail.com>
2018-10-19 11:35:55 -0500

    Server admin via site

--- commands/serverAdmin.js
+++ commands/serverAdmin.js
@@ -4,109 +4,14 @@ const Augur = require("augurbot"),
 const Module = new Augur.Module()
 .addCommand({name: "settings",
   description: "Update Server Settings",
-  syntax: "<botspam | prefix | accounce> <value>",
-  info: "Updates server-specific settings.\n**SERVER ADMINS ONLY**\n* botspam #channel | here | none\n* prefix !\n* announce #channel | here | none",
+  info: "Sends a link to updates server-specific settings (prefix, botspam, announce, role management).",
   aliases: ["setting", "set"],
   category: "Server Admin",
-  process: function(msg, suffix) {
-    let args = suffix.split(" ");
-
-    if (args.length < 2) {
-      msg.reply("you need to tell me both a setting and a value.")
-      .then(u.clean);
-      return;
-    }
-
-    let value, setting = args.shift().toLowerCase();
-
-    if ((setting == "botspam") || (setting == "spam")) {
-      // BOT SPAM
-      value = args.join(" ").trim().toLowerCase();
-      let channel = null;
-
-      if (msg.mentions.channels.size > 0) {
-        // SAVE BY MENTION
-        channel = msg.mentions.channels.first().id;
-      } else if ((value == "none") || (value == "false")) {
-        // REMOVE BOTSPAM
-        channel = "REMOVE";
-      } else {
-        // SAVE BY CHANNEL NAME
-        if (value == "here") {
-          channel = msg.channel.id;
-        } else {
-          channel = msg.guild.channels.find(c => c.name.toLowerCase() == value).id;
-        }
-      }
-
-      if (channel) {
-        channel = (channel != "REMOVE" ? channel : null);
-        if (channel && !(msg.guild.channels.get(channel) && msg.guild.channels.get(channel).permissionsFor(msg.client.user).has(["VIEW_CHANNEL","SEND_MESSAGES"]))) {
-          msg.reply(`I can't send messages to <#${channel}>`).then(u.clean).catch(console.error);
-        } else {
-          Module.db.server.saveSetting(msg.guild, "botspam", channel);
-          msg.channel.send("Botspam channel settings saved! :thumbsup:");
-        }
-      } else {
-        msg.reply("you need to tell me which channel to use.")
-        .then(u.clean).catch(console.error);
-      }
-    } else if ((setting == 'prefix') || (setting == 'command')) {
-      // PREFIX
-      value = args.join(" ").trim();
-      let userMentions = u.userMentions(msg);
-      if (userMentions && ((userMentions.size > 1) || ((userMentions.size == 1) && (userMentions.first().id != bot.user.id)))) {
-        msg.reply("you cannot set the command prefix to mention any user but me.").then(u.clean);
-      } else {
-        Module.db.server.saveSetting(msg.guild, 'prefix', value);
-        msg.channel.send("Prefix settings saved! :thumbsup:")
-        .then(u.clean).catch(console.error);
-      }
-    } else if ((setting == 'language') || (setting == 'locale')) {
-      // LANGUAGE
-      value = args.join(" ").trim().toUpperCase();
-      let locales = ["EN"];
-      if (locales.includes(value)) {
-        Module.db.server.saveSetting(msg.guild, "language", value);
-        msg.channel.send("Language settings saved! :thumbsup:")
-        .then(u.clean).catch(console.error);
-      } else {
-        msg.reply("Available languages include: " + locales.join(", "))
-        .then(u.clean).catch(console.error);
-      }
-    } else if (setting == "announce") {
-      // ANNOUNCE
-      value = args.join(" ").trim().toLowerCase();
-      let channel = null;
-
-      if (msg.mentions.channels.size > 0) {
-        // SAVE BY MENTION
-        channel = msg.mentions.channels.first().id;
-      } else if ((value == "none") || (value == "false")) {
-        // REMOVE ANNOUNCE
-        channel = "REMOVE";
-      } else {
-        // SAVE BY CHANNEL NAME
-        if (value == "here") {
-          channel = msg.channel.id;
-        } else {
-          channel = msg.guild.channels.find(c => c.name.toLowerCase() == value).id;
-        }
-      }
-
-      if (channel) {
-        channel = (channel != "REMOVE" ? channel : null);
-        if (channel && !(msg.guild.channels.get(channel) && msg.guild.channels.get(channel).permissionsFor(msg.client.user).has(["VIEW_CHANNEL","SEND_MESSAGES"]))) {
-          msg.reply(`I can't send messages to <#${channel}>`).then(u.clean).catch(console.error);
-        } else {
-          Module.db.server.saveSetting(msg.guild, "botspam", channel);
-          msg.channel.send("Announce channel settings saved! :thumbsup:");
-        }
-      } else {
-        msg.reply("you need to tell me which channel to use.")
-        .then(u.clean).catch(console.error);
-      }
-    }
+  process: async (msg) => {
+    try {
+      await msg.author.send(`Manage Gerard settings for your server here: <${Module.config.homePage}manage/${msg.guild.id}>`);
+      msg.channel.send("I sent you a link to your server management page.").then(u.clean);
+    } catch(e) { u.alertError(e, msg); }
   },
   permissions: (msg) => (msg.guild && (msg.member.permissions.has('MANAGE_GUILD') || msg.member.permissions.has('ADMINISTRATOR') || Module.config.adminId.includes(msg.author.id)))
 })
@@ -115,3 +20,106 @@ const Module = new Augur.Module()
 });
 
 module.exports = Module;
+
+/*
+function oldSettings(msg, suffix) {
+  let args = suffix.split(" ");
+
+  if (args.length < 2) {
+    msg.reply("you need to tell me both a setting and a value.")
+    .then(u.clean);
+    return;
+  }
+
+  let value, setting = args.shift().toLowerCase();
+
+  if ((setting == "botspam") || (setting == "spam")) {
+    // BOT SPAM
+    value = args.join(" ").trim().toLowerCase();
+    let channel = null;
+
+    if (msg.mentions.channels.size > 0) {
+      // SAVE BY MENTION
+      channel = msg.mentions.channels.first().id;
+    } else if ((value == "none") || (value == "false")) {
+      // REMOVE BOTSPAM
+      channel = "REMOVE";
+    } else {
+      // SAVE BY CHANNEL NAME
+      if (value == "here") {
+        channel = msg.channel.id;
+      } else {
+        channel = msg.guild.channels.find(c => c.name.toLowerCase() == value).id;
+      }
+    }
+
+    if (channel) {
+      channel = (channel != "REMOVE" ? channel : null);
+      if (channel && !(msg.guild.channels.get(channel) && msg.guild.channels.get(channel).permissionsFor(msg.client.user).has(["VIEW_CHANNEL","SEND_MESSAGES"]))) {
+        msg.reply(`I can't send messages to <#${channel}>`).then(u.clean).catch(console.error);
+      } else {
+        Module.db.server.saveSetting(msg.guild, "botspam", channel);
+        msg.channel.send("Botspam channel settings saved! :thumbsup:");
+      }
+    } else {
+      msg.reply("you need to tell me which channel to use.")
+      .then(u.clean).catch(console.error);
+    }
+  } else if ((setting == 'prefix') || (setting == 'command')) {
+    // PREFIX
+    value = args.join(" ").trim();
+    let userMentions = u.userMentions(msg);
+    if (userMentions && ((userMentions.size > 1) || ((userMentions.size == 1) && (userMentions.first().id != bot.user.id)))) {
+      msg.reply("you cannot set the command prefix to mention any user but me.").then(u.clean);
+    } else {
+      Module.db.server.saveSetting(msg.guild, 'prefix', value);
+      msg.channel.send("Prefix settings saved! :thumbsup:")
+      .then(u.clean).catch(console.error);
+    }
+  } else if ((setting == 'language') || (setting == 'locale')) {
+    // LANGUAGE
+    value = args.join(" ").trim().toUpperCase();
+    let locales = ["EN"];
+    if (locales.includes(value)) {
+      Module.db.server.saveSetting(msg.guild, "language", value);
+      msg.channel.send("Language settings saved! :thumbsup:")
+      .then(u.clean).catch(console.error);
+    } else {
+      msg.reply("Available languages include: " + locales.join(", "))
+      .then(u.clean).catch(console.error);
+    }
+  } else if (setting == "announce") {
+    // ANNOUNCE
+    value = args.join(" ").trim().toLowerCase();
+    let channel = null;
+
+    if (msg.mentions.channels.size > 0) {
+      // SAVE BY MENTION
+      channel = msg.mentions.channels.first().id;
+    } else if ((value == "none") || (value == "false")) {
+      // REMOVE ANNOUNCE
+      channel = "REMOVE";
+    } else {
+      // SAVE BY CHANNEL NAME
+      if (value == "here") {
+        channel = msg.channel.id;
+      } else {
+        channel = msg.guild.channels.find(c => c.name.toLowerCase() == value).id;
+      }
+    }
+
+    if (channel) {
+      channel = (channel != "REMOVE" ? channel : null);
+      if (channel && !(msg.guild.channels.get(channel) && msg.guild.channels.get(channel).permissionsFor(msg.client.user).has(["VIEW_CHANNEL","SEND_MESSAGES"]))) {
+        msg.reply(`I can't send messages to <#${channel}>`).then(u.clean).catch(console.error);
+      } else {
+        Module.db.server.saveSetting(msg.guild, "botspam", channel);
+        msg.channel.send("Announce channel settings saved! :thumbsup:");
+      }
+    } else {
+      msg.reply("you need to tell me which channel to use.")
+      .then(u.clean).catch(console.error);
+    }
+  }
+}
+*/
