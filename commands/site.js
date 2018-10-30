83614c14cb82d05d720562fc175b70117b57e68c
Gaiwecoor <gaiwecoor@gmail.com>
2018-10-30 10:14:27 -0500

    Site run on Shard 0 only

--- commands/site.js
+++ commands/site.js
@@ -26,59 +26,62 @@ const Module = new Augur.Module()
   permissions: (msg) => Module.config.adminId.includes(msg.author.id)
 })
 .setInit(() => {
-  app.set("views", "./site/views");
-  app.set("view engine", "pug");
-  app.disable("view cache");
-  app.use(session({
-    secret: config.sessionSecret,
-    cookie: { maxAge: 3600000 },
-    resave: false,
-    saveUninitialized: false
-  }));
-  app.use((req, res, next) => {
-    res.locals.handler = Module.handler;
-    res.locals.bot = Module.handler.client;
-    res.locals.loggedIn = (req.session.user || req.session.guilds);
-    next();
-  });
+  if (!Module.handler.client.shard || Module.handler.client.shard == 0) {
+    app.set("views", "./site/views");
+    app.set("view engine", "pug");
+    app.disable("view cache");
+    app.use(session({
+      secret: config.sessionSecret,
+      cookie: { maxAge: 3600000 },
+      resave: false,
+      saveUninitialized: false
+    }));
+    app.use((req, res, next) => {
+      res.locals.handler = Module.handler;
+      res.locals.bot = Module.handler.client;
+      res.locals.loggedIn = (req.session.user || req.session.guilds);
+      next();
+    });
 
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
 
-  let routerPath = path.resolve(process.cwd(), "./site/private");
-  let routers = fs.readdirSync(routerPath);
-  routers = routers.filter(r => r.endsWith(".js")).map(f => f.slice(0, -3));
+    let routerPath = path.resolve(process.cwd(), "./site/private");
+    let routers = fs.readdirSync(routerPath);
+    routers = routers.filter(r => r.endsWith(".js")).map(f => f.slice(0, -3));
 
-  routers.forEach(route => {
-    let router = require(path.resolve(routerPath, route));
-    if (route == "root") route = "";
-    app.use(`/${route}`, router);
-  });
+    routers.forEach(route => {
+      let router = require(path.resolve(routerPath, route));
+      if (route == "root") route = "";
+      app.use(`/${route}`, router);
+    });
 
-  app.use(Express.static("./site/public"));
+    app.use(Express.static("./site/public"));
 
-  // Assume 404'd
-  app.use((req, res) => {
-    res.render("pages/error", { status: 404, e: "Page not found." });
-  });
+    // Assume 404'd
+    app.use((req, res) => {
+      res.render("pages/error", { status: 404, e: "Page not found." });
+    });
 
-  http.listen(config.port, (err) => {
-    if (err) console.error(err);
-    else console.log("Listening on port", config.port);
-  });
-
+    http.listen(config.port, (err) => {
+      if (err) console.error(err);
+      else console.log("Listening on port", config.port);
+    });
+  }
 })
 .setUnload(() => {
-  http.close();
+  if (!Module.handler.client.shard || Module.handler.client.shard == 0) {
+    http.close();
 
-  let routerPath = path.resolve(process.cwd(), "./site/private");
-  let routers = fs.readdirSync(routerPath);
-  routers = routers.filter(r => r.endsWith(".js"));
+    let routerPath = path.resolve(process.cwd(), "./site/private");
+    let routers = fs.readdirSync(routerPath);
+    routers = routers.filter(r => r.endsWith(".js"));
 
-  routers.forEach(route => {
-    delete require.cache[require.resolve(path.resolve(routerPath, route))];
-  });
-  delete require.cache[require.resolve(path.resolve(process.cwd(), "./site/auth.js"))];
+    routers.forEach(route => {
+      delete require.cache[require.resolve(path.resolve(routerPath, route))];
+    });
+    delete require.cache[require.resolve(path.resolve(process.cwd(), "./site/auth.js"))];
+  }
 });
 
 module.exports = Module;
